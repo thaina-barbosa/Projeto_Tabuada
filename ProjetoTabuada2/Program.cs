aa35212489c1eddfdde5e41b6b8b643f8aa5205b
thaina-barbosa <thaina.mbarbosaa@gmail.com>
2022-07-18 14:17:43 -0300

    Adicionando Projeto Tabuada

---
+++ ProjetoTabuada2/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace ProjetoTabuada2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int numero, resultado;
+            
+            Console.WriteLine("Digite o Numero da Tabuada: ");
+            numero = Int16.Parse(Console.ReadLine());
+
+            for (int i = 1; i <= 10; i++)
+            {
+                resultado = numero * i;              
+                Console.WriteLine(String.Format("{0} x {1} = {2}", numero, i, resultado));
+
+                string variavelTeste = "teste";
+                Console.WriteLine(String.Format("Ola sou um {0}!", variavelTeste));
+            }
+            
+        }
+    }
+} 
